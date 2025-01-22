94d864c6b3bd15cc0f8281d4c80e553611f88c25
gschiavodev <gschiavo11@gmail.com>
2025-01-22 17:57:56 -0500

    use EST time

--- backend/project/views.py
+++ backend/project/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
 from datetime import datetime
+import pytz
 
 def index(request):
-    current_time = datetime.now().strftime("%-I:%S %p")
-    current_date = datetime.now().strftime("%A %m, %-Y")
+    # Set the timezone to your local timezone
+    local_tz = pytz.timezone('America/New_York')  # Replace with your timezone
+    current_time = datetime.now(local_tz).strftime("%I:%M %p")
+    current_date = datetime.now(local_tz).strftime("%A %B %d, %Y")
 
     data = {
         "time": current_time,
@@ -11,4 +14,3 @@ def index(request):
     }
 
     return JsonResponse(data)
-
