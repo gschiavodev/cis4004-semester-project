1dceab766a3150b8e192afd19e202d8d4ad7cf4d
gschiavodev <gschiavo11@gmail.com>
2025-02-15 07:32:04 -0500

    MUI theme + user forms

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          My Application
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
