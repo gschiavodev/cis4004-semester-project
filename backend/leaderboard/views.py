3de637a301151d19a075cd6de75458be7e524d36
TokyoWavez <tokyowavez@gmail.com>
2025-03-06 13:39:32 -0500

    Update views.py

--- backend/leaderboard/views.py
+++ backend/leaderboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Leaderboard
 
-# Create your views here.
+def leaderboard_view(request):
+    # Get the top 10 players ordered by score
+    leaderboard = Leaderboard.objects.all().order_by('-score', 'timestamp')[:10]
+
+    return render(request, 'leaderboard/leaderboard.html', {'leaderboard': leaderboard})
