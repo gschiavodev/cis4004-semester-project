2c831887e486e5ec0a860fb56513b5fc35c2e740
gabriel <>
2025-03-07 11:28:41 -0500

    Revert " models.py"
    This reverts commit eba04f14ede138a3ae506e2dce0b5358a6d2c948.

--- backend/leaderboard/models.py
+++ backend/leaderboard/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-class Leaderboard(models.Model):
-    username = models.CharField(max_length=100)
-    score = models.IntegerField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.username}: {self.score} points"
+# Create your models here.
