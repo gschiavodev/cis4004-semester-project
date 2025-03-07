8a7f49e1ee3ef4b8db75239584cc59409e6ee845
gabriel <>
2025-03-07 11:28:35 -0500

    Revert "Update views.py"
    This reverts commit 3de637a301151d19a075cd6de75458be7e524d36.

--- backend/leaderboard/views.py
+++ backend/leaderboard/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
-from .models import Leaderboard
 
-def leaderboard_view(request):
-    # Get the top 10 players ordered by score
-    leaderboard = Leaderboard.objects.all().order_by('-score', 'timestamp')[:10]
-
-    return render(request, 'leaderboard/leaderboard.html', {'leaderboard': leaderboard})
+# Create your views here.
